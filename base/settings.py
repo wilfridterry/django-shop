9e6d14a648d0275bc6529602684beac0e3184aa7
Stannis <zalevschi1998@gmail.com>
2023-11-06 21:26:47 +0200

    Added a custom 404 page

--- base/settings.py
+++ base/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b)ygz^f+l9aue1#30n6+y9loz64(7v(%mn4edf39x54m@g6++r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
--- base/views.py
+++ base/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 def my_custom_not_found_view(request: HttpRequest, exception: Exception) -> HttpResponse:
-    return render(request, '404.html', status=404)
+    return render(request, 'error_handlers/404.html', status=404)
